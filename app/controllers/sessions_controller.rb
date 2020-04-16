849a7b9e5e6f315af40a9d635071bf53c5139967
prashanth-23 <prashanthreddy9948379663@gmail.com>
2020-04-16 13:55:35 +0530

    added basic code in controllers and views for users,sessions,homepage,added email and password to users table initialised routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      flash[:success] = "logged in successfully"
+      session[:current_user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:error] = "Invalid credentials please try again"
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
+end
