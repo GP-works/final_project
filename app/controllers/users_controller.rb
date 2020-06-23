b86a68152efc9a0a46df529b75da480609bb7f29
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-06-23 21:34:36 +0530

    ensured user logged in some methods of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :ensure_user_logged_in
+  before_action :ensure_user_logged_in, only: [:edit, :show, :destroy]
 
   def new
   end
