aaedbca648cb502cb2312b4be2c791fab384992e
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-06-23 17:54:10 +0530

    Added Logo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :orders, dependent: :destroy
   validates :name, presence: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4 }, on: :create
 
   def self.customers
     where("role = ?", "customer")
